fb6740857b854ecf325c0c761847a03aa01bc2d3
AlumnosInf <AlumnosInf@soportetiu.local>
2023-05-12 12:26:11 -0400

    lab05

--- laboratorios/INFJMC/core/admin.py
+++ laboratorios/INFJMC/core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from core.models import carrera,docente
+
+class CarreraAdmin (admin.ModelAdmin):
+    pass
+
+admin.site.register(carrera,CarreraAdmin)
+
+class DocenteAdmin (admin.ModelAdmin):
+    pass
+
+admin.site.register(docente,DocenteAdmin)
--- laboratorios/INFJMC/core/views.py
+++ laboratorios/INFJMC/core/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import carrera
 
 
 def index(request):
     return render(request,"core/home.html")
 # Create your views here.
 
-def carrera(request):
+def Carrera(request):
+    carreras = carrera.objects.all()
     return render(request,"core/carreras.html")
 
 def profesores(request):
